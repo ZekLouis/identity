f28baf5c8faafb6325b83a59d7c4de32df05d643
Louis Gaume <Louis Gaume>
2017-04-01 12:46:38 +0200

    Messages, système retour, nouvelles animations

--- js/script.js
+++ js/script.js
@@ -1,21 +1,86 @@
+function successMessage(message){
+  $(".isa_success").slideDown(300);
+  $(".isa_success>span").text(message)
+
+  setTimeout(function(){
+    $(".isa_success").slideUp(300);
+  }, 3000)
+}
+
+function errorMessage(message){
+  $(".isa_error").slideDown(300);
+  $(".isa_error>span").text(message)
+
+  setTimeout(function(){
+    $(".isa_error").slideUp(300);
+  }, 3000)
+}
+
 $(function(){
   // Si on voit trop les autres menus
   $("#coffre").hide()
   $("#portes").hide()
+  $(".isa").hide()
 
-  $(".btn:not(:has(.ret))").click(function(){
+  $(".btn").not(".ret").click(function(){
+    console.log("zaereazr");
+      switch ($(this).data('link')) {
+        case 'close':
+          $(".block").hide('slide',{direction:'right'},500)
+          break;
 
-      if($(this).data('link')){
+        case 'verVeh':
+          successMessage("Vous avez verrouillé votre véhicule")
+          break;
 
-        $(".active").hide('slide',{direction:'right'},500)
-        $(".active").removeClass("active")
-        var obj = $(this)
-        setTimeout(function(){
-          $("#"+obj.data('link')).show('slide',{direction:'right'},500);
-        }, 600);
-        $("#"+$(this).data('link')).addClass("active")
+        case 'putObj':
+          errorMessage("Vous n'avez pas d'objets ou d'armes sur vous")
+          break;
 
+        case 'getObj':
+          errorMessage("Le coffre est vide")
+          break;
+
+        case 'watchTrunk':
+          successMessage("Le coffre est vide")
+          break;
+
+        case 'trunk':
+          successMessage("Vous avez ouvert le coffre")
+          break;
+
+        case 'hood':
+          successMessage("Vous avez ouvert le capot")
+          break;
+
+        case 'doorAVLeft':
+          successMessage("Vous avez ouvert la porte")
+          break;
+
+        case 'doorAVRight':
+          successMessage("Vous avez ouvert la porte")
+          break;
+
+        case 'doorARLeft':
+          successMessage("Vous avez ouvert la porte")
+          break;
+
+        case 'doorARRight':
+          successMessage("Vous avez ouvert la porte")
+          break;
+
+        default:
+          $(".active").hide('slide',{direction:'left'},500)
+          $(".active").removeClass("active")
+          var obj = $(this)
+          setTimeout(function(){
+            $("#"+obj.data('link')).show('slide',{direction:'right'},500);
+          }, 600);
+          $("#"+$(this).data('link')).addClass("active")
+          break;
       }
+
+
 
   })
 
@@ -23,7 +88,7 @@ $(function(){
     $(".active").hide('slide',{direction:'right'},500)
     $(".active").removeClass("active")
     setTimeout(function(){
-      $("#main").show('slide',{direction:'right'},500)      
+      $("#main").show('slide',{direction:'left'},500)
     }, 600)
     $("#main").addClass("active")
   })
